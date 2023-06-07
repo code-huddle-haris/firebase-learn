ce1cf88c7a6c59b2ed4d0f853993b937b58c689e
code-huddle-haris <haris.ahmed@code-huddle.com>
2023-06-07 17:16:36 +0500

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgKkeceVvlBUVYSm-Ju8kPtiAfNbGn9tw",
+  authDomain: "gymyg-staging.firebaseapp.com",
+  projectId: "gymyg-staging",
+  storageBucket: "gymyg-staging.appspot.com",
+  messagingSenderId: "616518155239",
+  appId: "1:616518155239:web:98add7b2f3df6af9d01b42",
+  measurementId: "G-ZMCKBMWYDN",
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  // eslint-disable-next-line no-restricted-globals
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
