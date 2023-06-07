2627799cfe2ac742650ed30d17948e30a36f41f9
code-huddle-haris <haris.ahmed@code-huddle.com>
2023-06-07 17:37:15 +0500

    dsad

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,21 +1,15 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("../firebase-messaging-sw.js")
+    .then(function (registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function (err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAgKkeceVvlBUVYSm-Ju8kPtiAfNbGn9tw",
-  authDomain: "gymyg-staging.firebaseapp.com",
-  projectId: "gymyg-staging",
-  storageBucket: "gymyg-staging.appspot.com",
-  messagingSenderId: "616518155239",
-  appId: "1:616518155239:web:98add7b2f3df6af9d01b42",
-  measurementId: "G-ZMCKBMWYDN",
-};
-
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
+// eslint-disable-next-line no-undef
 messaging.onBackgroundMessage((payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message ",
