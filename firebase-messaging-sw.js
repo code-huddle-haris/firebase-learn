38c736ed2c2c65b1e8613eb2b3fbd35ff24b954f
code-huddle-haris <haris.ahmed@code-huddle.com>
2023-06-08 11:49:42 +0500

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,7 @@
-importScripts("https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js");
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("../firebase-messaging-sw.js")
-    .then(function (registration) {
-      console.log("Registration successful, scope is:", registration.scope);
-    })
-    .catch(function (err) {
-      console.log("Service worker registration failed, error:", err);
-    });
-}
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
 
 const firebaseConfig = {
   apiKey: "AIzaSyAgKkeceVvlBUVYSm-Ju8kPtiAfNbGn9tw",
@@ -23,14 +14,11 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
